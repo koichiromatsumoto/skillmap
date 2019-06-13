209437dfa5b0fdfd9db5222ab41f0cb11077a301
林 克明 <hayashi-k@i-sol.jp>
2019-06-13 14:34:11 +0900

    回答結果ページに戻るボタン追加

--- app/controllers/front/answers_controller.rb
+++ app/controllers/front/answers_controller.rb
@@ -10,8 +10,8 @@ class Front::AnswersController < Front::BaseController
     last_answer = current_user.answers.last
 
     @answers = [].tap do |list|
-      Layer.includes(:user).references(:user).where(User.c[:id].eq(nil).or(User.arel_table[:id].eq(current_user.id))).each do |layer|
-        detail = last_answer.details.find { |d| d.layer.id == layer.id }
+        Layer.includes(:user).references(:user).where(User.arel_table[:id].eq(nil).or(User.arel_table[:id].eq(current_user.id))).each do |layer|
+        detail = last_answer&.details&.find { |d| d.layer.id == layer.id }
         list << AnswerDetail.new(layer_id: layer.id, score: detail&.score || 0)
       end
     end
